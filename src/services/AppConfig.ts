37d1a48c7e97f1a730e936075ded20849635936f
Sten Muchow <sten.muchow@gmail.com>
2024-01-24 09:50:10 -0500

    fix api

--- src/services/AppConfig.ts
+++ src/services/AppConfig.ts
@@ -2,10 +2,9 @@ type AppConfig = {
   apiBaseUrl: string;
 };
 
-const useLocalApi = true;
-
 export const appConfig: AppConfig = {
-  apiBaseUrl: useLocalApi
-    ? "http://localhost:1337"
-    : process.env.NEXT_PUBLIC_API_URL,
+  apiBaseUrl:
+    process.env.USE_LOCAL_API === "true"
+      ? "http://localhost:1337"
+      : process.env.NEXT_PUBLIC_API_URL,
 };
